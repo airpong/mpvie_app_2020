dfb7df825352bea14e03cac0c3c71e105db5b7f9
Changun Ha <giponge@gmail.com>
2020-05-18 22:03:30 +0900

    Updates

---
+++ precache-manifest.98d94fe052cda38544beb1546ef2d8d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f28719f3f8f4051c3da0a537b242b90",
+    "url": "/mpvie_app_2020/index.html"
+  },
+  {
+    "revision": "9ef30c42f53b42079977",
+    "url": "/mpvie_app_2020/static/css/main.caf39c13.chunk.css"
+  },
+  {
+    "revision": "987f52152c3e727affbf",
+    "url": "/mpvie_app_2020/static/js/2.ded08cb9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mpvie_app_2020/static/js/2.ded08cb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ef30c42f53b42079977",
+    "url": "/mpvie_app_2020/static/js/main.53d7bf5b.chunk.js"
+  },
+  {
+    "revision": "b6cf32e567d49e5d5298",
+    "url": "/mpvie_app_2020/static/js/runtime-main.83d05cd6.js"
+  }
+]);
